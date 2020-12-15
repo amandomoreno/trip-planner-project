b0d6742cec0971613f38b9706d37003a874ff91e
amandomoreno <amandomoreno47@gmail.com>
2020-12-15 08:57:49 -0600

    add destinations

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -6,17 +6,44 @@ module.exports = {
 }
 
 function newDestination(req, res) {
-    res.render('destinations/new', {title: "Add Destination", err: ''})
+    res.render('destinations/new', {
+        title: "Add Destination", 
+        user: req.user,
+        err: ''})
 } 
 
 function create(req, res){
-    const destination = new Destination(req.body)
+    const destination = new Destination(req.body.postedBy)
+    req.body.postedBy = req.user.name
+    req.body.avatar = req.user.avatar
     destination.save(function(err){
         if (err){
         console.log(err)
         return res.redirect('/destinations/new')
         }
         console.log(destination)
+        Destination.create(req.body)
         res.redirect(`/destinations/${destination._id}`)
     })
 }
+
+// function create(req, res) {
+//     req.body.postedBy = req.user.name
+//     req.body.avatar = req.user.avatar
+//     Message.create(req.body)
+//     .then(() => {
+//       res.redirect('/messages')
+//     })
+//   }
+
+
+// function index(req, res) {
+//     Message.find({})
+//     .then((messages) => {
+//       res.render("messages/index", {
+//         user: req.user,
+//         title: "Message Board",
+//         messages: messages.reverse()
+//       })
+//     })
+//   }
